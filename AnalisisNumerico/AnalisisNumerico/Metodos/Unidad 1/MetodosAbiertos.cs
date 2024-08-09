80042425e926176762c44f3f4560ba37201c8f87
Santiago <santiandermatten@gmail.com>
2024-08-09 01:34:30 -0300

    Adicion de Regla Falsa y de NewtonRaphson

---
+++ AnalisisNumerico/AnalisisNumerico/Metodos/Unidad 1/MetodosAbiertos.cs
@@ -0,0 +1,58 @@
+﻿using AnalisisNumericos;
+using Calculus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalisisNumerico.Metodos.Unidad_1
+{
+    public class MetodosAbiertos
+    {
+        public Resultado MetodoNewtonRaphson(string funcion, double tolerancia, int cantidaditeraciones)
+        {
+            //Inicializamos las variables
+            Resultado rta = new Resultado();
+            Calculo analizadorFuncion = new Calculo();
+            int contadoriteraciones = 0;
+            double xanterior = 0;
+            Console.WriteLine("Ingrese el valor de xi");
+            double xi = double.Parse(Console.ReadLine());
+            double valorabsoluto = Math.Abs(analizadorFuncion.EvaluaFx(xi)); //Por acá habría que comenzar un while, el tema es con que condicion
+            if (valorabsoluto < tolerancia) {  //Esto en caso de que xi sea la raíz de una
+            rta.ValorXr = xi;
+                rta.Sucess = true;
+                rta.Converge = true;
+                rta.ErrorRelativo = 0;
+                rta.CantidadIteraciones = contadoriteraciones;
+                return rta;
+            }
+            contadoriteraciones++;
+            double derivada = ((analizadorFuncion.EvaluaFx(xi) + tolerancia ) - analizadorFuncion.EvaluaFx(xi)) / tolerancia;
+            if (derivada == 0) {
+                Console.WriteLine("No se encuentra la Raíz");
+                rta.Sucess = false; 
+                rta.Converge = false;
+                rta.ValorXr = 0; //El valor sería nulo?
+                rta.ErrorRelativo = 0; //Hay que ver el calculo del error relativo ((xri+1 - xri) / xri+1)
+                return rta;
+            }
+            double xr = xi - (analizadorFuncion.EvaluaFx(xi) / derivada);
+            double Error = 0; //((abs(xr - xant)) / xr)  ???
+            if (Math.Abs(analizadorFuncion.EvaluaFx(xr)) < tolerancia || Error < tolerancia || cantidaditeraciones < contadoriteraciones)
+            {
+                rta.ValorXr = xr;
+                rta.ErrorRelativo = 0;
+                rta.Sucess = true;
+                rta.Converge = true;
+                rta.CantidadIteraciones = contadoriteraciones;
+                return rta;
+            }
+            xanterior = xr;
+            xi = xr; //acá volvería hasta arriba de donde calcula la derivada, pero el tema es que condición ponerle al while
+            return rta; 
+        }
+    }
+}
--- AnalisisNumerico/AnalisisNumerico/Metodos/Unidad 1/MetodosCerrados.cs
+++ AnalisisNumerico/AnalisisNumerico/Metodos/Unidad 1/MetodosCerrados.cs
@@ -73,5 +73,74 @@ namespace AnalisisNumerico.Metodos.Unidad_1
             }
             return resultado;
         }
+
+        //Hay que chequear que hay que corregir en este
+        public Resultado UseReglaFalsa(String funcion, double tolerancia, int cantidadIteraciones)
+        {
+            Resultado resultado = new Resultado();
+            Calculo analizadorFuncion = new Calculo();
+            double xrAnterior = 0;
+            int contadorIteraciones = 0;
+
+            Console.WriteLine("Ingrese el valor de xi:");
+            double xi = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de xd:");
+            double xd = double.Parse(Console.ReadLine());
+
+            // Lógica para cuando se ingresa el intervalo y uno de los extremos es raíz o los extremos no contienen la raíz.
+            while (analizadorFuncion.EvaluaFx(xi) * analizadorFuncion.EvaluaFx(xd) >= 0)
+            {
+                if (analizadorFuncion.EvaluaFx(xi) * analizadorFuncion.EvaluaFx(xd) == 0)
+                {
+                    resultado.ValorXr = analizadorFuncion.EvaluaFx(xi) == 0 ? xi : xd;
+                    resultado.ErrorRelativo = 0;
+                    resultado.CantidadIteraciones = contadorIteraciones;
+                    resultado.Sucess = true;
+                    break;
+                }
+                Console.WriteLine($"El intervalo [{xi}, {xd}] no contiene a la raíz. Ingrese nuevamente el intervalo.");
+                Console.WriteLine("Ingrese el valor de xi:");
+                xi = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese el valor de xd:");
+                xd = double.Parse(Console.ReadLine());
+            }
+
+            if (resultado.Sucess) return resultado;
+
+            // Lógica para hacer el método de la Regla Falsa en sí.
+            while (true)
+            {
+                contadorIteraciones++;
+                double fxi = analizadorFuncion.EvaluaFx(xi);
+                double fxd = analizadorFuncion.EvaluaFx(xd);
+                double xr = xd - (fxd * (xd - xi)) / (fxd - fxi);
+                double fxr = analizadorFuncion.EvaluaFx(xr);
+
+                double error = Math.Abs((xr - xrAnterior) / xr);
+
+                if (fxr < tolerancia || contadorIteraciones > cantidadIteraciones || error < tolerancia)
+                {
+                    resultado.ValorXr = xr;
+                    resultado.ErrorRelativo = error;
+                    resultado.CantidadIteraciones = contadorIteraciones;
+                    resultado.Sucess = true;
+                    break;
+                }
+                else
+                {
+                    if (fxi * fxr < 0)
+                    {
+                        xd = xr;
+                    }
+                    else
+                    {
+                        xi = xr;
+                    }
+                    xrAnterior = xr;
+                }
+            }
+
+            return resultado;
+        }
     }
 }
